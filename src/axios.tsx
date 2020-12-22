8d8d4b3b8249d092682b08165afcc6357bbb6adf
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-12-22 16:49:23 -0500

    changin ulr from api

--- src/axios.tsx
+++ src/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance_discord = axios.create({
-    baseURL: 'https://discordnodejsexpress.herokuapp.com'
+    baseURL: 'https://discordnodeexpressjs.herokuapp.com/'
 });
 
 export default instance_discord;
